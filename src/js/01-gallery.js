b364cf12ab7d079d64f6b61732ff8475b332f262
Igor Pryntsypal <igorprincipal@gmail.com>
2023-11-05 18:06:14 +0200

    Initial commit

---
+++ src/js/01-gallery.js
@@ -0,0 +1,14 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { createMarkup } from './helpers';
+
+const container = document.querySelector('.gallery');
+container.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
+
+new SimpleLightbox('.gallery__item a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
---
+++ src/js/03-feedback.js
@@ -0,0 +1,36 @@
+import { getFromLS, saveTolS } from './helpers';
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', throttle(inInput, 500));
+form.addEventListener('submit', onSubmit);
+
+function inInput(evt) {
+  const key = evt.target.name;
+  const value = evt.target.value;
+  saveTolS(key, value);
+}
+
+function onStart() {
+  const email = getFromLS('email');
+  const message = getFromLS('message');
+
+  form.elements.email.value = email;
+  form.elements.message.value = message;
+}
+onStart();
+
+function onSubmit(evt) {
+  evt.preventDefault();
+  const email = form.elements.email.value;
+  const message = form.elements.message.value;
+  const info = {
+    email,
+    message,
+  };
+  console.log(info);
+  form.reset();
+  localStorage.removeItem('email');
+  localStorage.removeItem('message');
+}
